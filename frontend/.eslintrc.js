7a9bb26bcfbdf922d25966820f114171731308aa
Mohamed Ismail <mohammadismail.1993@gmail.com>
2025-07-06 02:07:43 +0300

    added frontend ignorable files to .gitignore .. fixed a frontend compile error related to Eslint dependency

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'react-app/jest'
+  ],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    // Add any custom rules here if needed
+  },
+  overrides: [
+    {
+      files: ['**/*.test.js', '**/*.test.jsx', '**/*.spec.js', '**/*.spec.jsx'],
+      env: {
+        jest: true
+      }
+    }
+  ]
+}; 
